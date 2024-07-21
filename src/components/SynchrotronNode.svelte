30d7c0e141e45c53a1878dd339cff0ff1c00d595
ThatOtherAndrew <stroev.andrew@gmail.com>
2024-07-21 19:09:22 +0200

    Improve styling/appearance, fix overlapping handles and show handle labels

--- src/components/SynchrotronNode.svelte
+++ src/components/SynchrotronNode.svelte
@@ -17,55 +17,97 @@
 </script>
 
 <div class="node">
-    <div class="header">
-        <strong>{$nodeData.type}</strong> {$nodeData.name}
+    <div class="handle-container inputs">
+        {#each $nodeData.inputs as input}
+            <Handle id={input.port_name} type="target" position={Position.Left} class="handle">
+                <span class="handle-label">{input.port_name}</span>
+            </Handle>
+        {/each}
     </div>
-    {#if (Object.keys($nodeData.exports).length)}
-        <table class="exports">
-            {#each Object.entries($nodeData.exports) as [key, value]}
-                <tr>
-                    <th>{key}</th>
-                    <td>&nbsp;=&nbsp;</td>
-                    <td>{value}</td>
-                </tr>
-            {/each}
-        </table>
-    {/if}
+    <div class="handle-container outputs">
+        {#each $nodeData.outputs as output}
+            <Handle id={output.port_name} type="source" position={Position.Right} class="handle">
+                <span class="handle-label">{output.port_name}</span>
+            </Handle>
+        {/each}
+    </div>
 
-    {#each $nodeData.inputs as input}
-        <Handle id={input.port_name} type="target" position={Position.Left} />
-    {/each}
-    {#each $nodeData.outputs as output}
-        <Handle id={output.port_name} type="source" position={Position.Right} />
-    {/each}
+    <div class="node-content">
+        <div class="header">
+            <strong>{$nodeData.type}</strong> {$nodeData.name}
+        </div>
+        {#if (Object.keys($nodeData.exports).length)}
+            <table class="exports">
+                {#each Object.entries($nodeData.exports) as [key, value]}
+                    <tr>
+                        <th>{key}</th>
+                        <td>&nbsp;=&nbsp;</td>
+                        <td>{value}</td>
+                    </tr>
+                {/each}
+            </table>
+        {/if}
+    </div>
 </div>
 
 <style lang="scss">
     .node {
-        color: var(--xy-node-color-default);
-        background-color: #eee;
+        color: var(--colour-fg-default);
+        background-color: var(--colour-bg-default);
         border-radius: .3rem;
-        font-family: 'JetBrains Mono', monospace;
         font-size: .7rem;
         overflow: clip;
         width: fit-content;
+        height: fit-content;
+    }
 
-        > * {
-            padding: .3em 1em;
-        }
+    .node-content > * {
+        padding: .3em 1em;
     }
 
     .header {
-        background-color: #ccc;
+        background-color: var(--colour-bg-accent);
     }
 
-    :global(.svelte-flow.dark) {
-        .node {
-            background-color: #333
-        }
+    .handle-container {
+        position: absolute;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
 
-        .header {
-            background-color: #555;
+        :global(.handle) {
+            position: static;
+            margin: 5px 0;
+        }
+    }
+
+    .handle-label {
+        font-size: .5rem;
+        color: var(--colour-fg-muted);
+        position: absolute;
+        bottom: .5em;
+        font-style: italic;
+        letter-spacing: -.05em;
+    }
+
+    .inputs {
+        left: 0;
+        :global(.handle) {
+            transform: translate(-50%, 0);
+        }
+        .handle-label {
+            right: 1em;
+        }
+    }
+
+    .outputs {
+        right: 0;
+        :global(.handle) {
+            transform: translate(50%, 0);
+        }
+        .handle-label {
+            left: 1em;
         }
     }
 </style>
