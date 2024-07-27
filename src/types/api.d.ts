c57855437d776b18df9dd82dca768380c1306557
ThatOtherAndrew <stroev.andrew@gmail.com>
2024-07-27 21:46:20 +0100

    Replace GET with POST at /execute endpoint

--- src/types/api.d.ts
+++ src/types/api.d.ts
@@ -4,6 +4,23 @@
  */
 
 export interface paths {
+    "/execute": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get?: never;
+        put?: never;
+        /** Execute */
+        post: operations["execute_execute_post"];
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
     "/start": {
         parameters: {
             query?: never;
@@ -110,23 +127,6 @@ export interface paths {
         patch?: never;
         trace?: never;
     };
-    "/execute": {
-        parameters: {
-            query?: never;
-            header?: never;
-            path?: never;
-            cookie?: never;
-        };
-        /** Execute */
-        get: operations["execute_execute_get"];
-        put?: never;
-        post?: never;
-        delete?: never;
-        options?: never;
-        head?: never;
-        patch?: never;
-        trace?: never;
-    };
 }
 export type webhooks = Record<string, never>;
 export interface components {
@@ -205,6 +205,26 @@ export interface components {
 }
 export type $defs = Record<string, never>;
 export interface operations {
+    execute_execute_post: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        requestBody?: never;
+        responses: {
+            /** @description Successful Response */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "application/json": string;
+                };
+            };
+        };
+    };
     start_rendering_start_get: {
         parameters: {
             query?: never;
@@ -464,26 +484,6 @@ export interface operations {
                 };
                 content: {
                     "application/json": unknown;
-                };
-            };
-        };
-    };
-    execute_execute_get: {
-        parameters: {
-            query?: never;
-            header?: never;
-            path?: never;
-            cookie?: never;
-        };
-        requestBody?: never;
-        responses: {
-            /** @description Successful Response */
-            200: {
-                headers: {
-                    [name: string]: unknown;
-                };
-                content: {
-                    "application/json": string;
                 };
             };
         };
