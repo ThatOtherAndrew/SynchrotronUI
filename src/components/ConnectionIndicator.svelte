3af36e9add28d3c6a562d1762d819253ceb12930
Andromeda <stroev.andrew@gmail.com>
2025-10-19 00:18:26 +0100

    Connection state animation which took way too long

--- src/components/ConnectionIndicator.svelte
+++ src/components/ConnectionIndicator.svelte
@@ -4,6 +4,7 @@
 
 <script lang="ts">
     import { Panel } from '@xyflow/svelte';
+    import { fly } from 'svelte/transition';
 
     let { connectionState }: { connectionState: ConnectionState } = $props();
 
@@ -15,7 +16,17 @@
 <Panel position="top-left">
     <div class="indicator {connectionState}">
         <span class="circle"></span>
-        <span class="label">{displayConnectionState}</span>
+        <div class="label-container">
+            {#key connectionState}
+                <span
+                    class="label"
+                    in:fly={{ x: -10, duration: 200, delay: 150 }}
+                    out:fly={{ x: 10, duration: 150 }}
+                >
+                    {displayConnectionState}
+                </span>
+            {/key}
+        </div>
     </div>
 </Panel>
 
@@ -28,13 +39,15 @@
         gap: 0.5em;
         padding: 0.4em 0.8em;
         border-radius: 999em;
+        overflow: hidden;
 
         color: lightgrey;
         background-color: rgba(dimgrey, 0.75);
         backdrop-filter: blur(4px);
         transition:
             color 0.5s ease,
-            background-color 0.5s ease;
+            background-color 0.5s ease,
+            width 0.3s ease;
 
         &.connected {
             color: limegreen;
@@ -52,5 +65,13 @@
         height: 0.75em;
         border-radius: 50%;
         background: currentColor;
+    }
+
+    .label-container {
+        display: grid;
+
+        > * {
+            grid-area: 1 / 1;
+        }
     }
 </style>
